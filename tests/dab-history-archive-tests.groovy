7e5355a2c2e56096a33977a3551cd83b3f2ea593
mibach-crypto <mibach@hawaiianexperiencespa.com>
2025-08-27 13:34:57 -0700

    feat: archive DAB history

---
+++ tests/dab-history-archive-tests.groovy
@@ -0,0 +1,42 @@
+package bot.flair
+
+import me.biocomp.hubitat_ci.util.CapturingLog
+import me.biocomp.hubitat_ci.api.app_api.AppExecutor
+import me.biocomp.hubitat_ci.app.HubitatAppSandbox
+import me.biocomp.hubitat_ci.validation.Flags
+import spock.lang.Specification
+
+class DabHistoryArchiveTests extends Specification {
+
+  private static final File APP_FILE = new File('src/hubitat-flair-vents-app.groovy')
+  private static final List VALIDATION_FLAGS = [
+    Flags.DontValidateMetadata,
+    Flags.DontValidatePreferences,
+    Flags.DontValidateDefinition,
+    Flags.DontRestrictGroovy,
+    Flags.DontRequireParseMethodInDevice
+  ]
+
+  def setup() {
+    new File('data').deleteDir()
+  }
+
+  def "activity logs are archived and retrievable"() {
+    setup:
+    final log = new CapturingLog()
+    AppExecutor executorApi = Mock {
+      _ * getState() >> [:]
+      _ * getLog() >> log
+    }
+    def sandbox = new HubitatAppSandbox(APP_FILE)
+    def script = sandbox.run('api': executorApi, 'validationFlags': VALIDATION_FLAGS)
+    script.location = [timeZone: TimeZone.getTimeZone('UTC')]
+
+    when:
+    script.appendDabActivityLog('testing archive')
+
+    then:
+    def history = script.readDabHistoryArchive()
+    history.any { it.type == 'activity' && it.message == 'testing archive' }
+  }
+}
